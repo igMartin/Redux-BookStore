66cca72b8bf5d2bcebb9a0ce06153da554856c28
igMartin <martin.ignacio@hotmail.com>
2017-01-26 00:39:03 -0300

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The goog parts' },
+    { title: 'Eloquent Javascript' },
+    { title: 'You dont know JS' },
+    { title: 'Speaking Javascript' }
+  ]
+}
