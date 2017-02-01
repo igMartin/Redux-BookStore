776b031ac66277bfed75455efac9e87962996eda
igMartin <ignacio.martin@hotmail.com>
2017-01-31 22:26:14 -0300

    testing git

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,6 @@
 export default function() {
   return [
-    { title: 'Javascript: The goog parts' },
+    { title: 'Javascript: The good parts' },
     { title: 'Eloquent Javascript' },
     { title: 'You dont know JS' },
     { title: 'Speaking Javascript' }
